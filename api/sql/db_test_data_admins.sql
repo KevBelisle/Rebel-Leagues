9bf4da08b804e983d9901c66443e0a1c4c9d2a86
Kevin Belisle <kev.belisle@gmail.com>
2014-03-04 01:03:30 -0500

    Added test entries for admins table

---
+++ api/sql/db_test_data_admins.sql
@@ -0,0 +1,9 @@
+
+
+/* POPULATE admins
+============================================= */
+
+INSERT INTO admins (admin_id, username, password, salt, tier) VALUES (1, "Tier1", "28a76a5d39aff3879454cdaff449d650bcb45b9250ccb568c07d3e88bbe4827147dad911640e5d0e1efb98228e10b735bc393239403edcb750cfd969a1705556", "salt", 1);
+INSERT INTO admins (admin_id, username, password, salt, tier) VALUES (2, "Tier2", "0ad33655d7b4d1cd02effbe12bdc691144de6e1be7a7622f4cc02255b58de24c6c0bee7b9811cacccf9e33e6ecd1ec8ab016393ee50c86af1b66e15e78e94ad9", "359d4f4db192febb6b09", 2);
+INSERT INTO admins (admin_id, username, password, salt, tier) VALUES (3, "Tier3", "f268fcde6d97190a19ed19e9535497e66aa1eb00097a531fab827248a984d59ab98ff9994bcb6c2a6fee208cd6f257c12bd87e8990cdd337664d110ef59db32d", "7c4827f59e7ba3c5176c", 3);
+
