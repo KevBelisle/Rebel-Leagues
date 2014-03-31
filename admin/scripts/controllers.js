390b56692fc4f7494eeecc202b614611aadf57e8
Kevin Belisle <kev.belisle@gmail.com>
2014-03-31 11:24:19 -0400

    Added login check on admin load.

--- admin/scripts/controllers.js
+++ admin/scripts/controllers.js
@@ -15,6 +15,15 @@ rebelLeaguesAdminControllers.controller('mainCtrl', ['$scope', '$http',
 			tier: 5
 		};
 		
+		$http.get("../api/login").success(function(data) {
+			console.log(data);
+			if (data.status == "success") {
+				$scope.loggedIn = true;
+				$scope.user.username = data.data.username;
+				$scope.user.tier = data.data.tier;
+			}
+		});
+		
 		$scope.loginInfo = {
 			username: "",
 			password: "",
@@ -178,6 +187,8 @@ rebelLeaguesAdminControllers.controller('addPlayerCtrl', ['$scope', '$http',
 			$http.post('../api/players', player).success( function (data) {
 				alert(data);
 				console.log(data);
+			}).error( function(data) {
+				console.log(data);
 			});
 		
 			console.log(player);
@@ -186,6 +197,99 @@ rebelLeaguesAdminControllers.controller('addPlayerCtrl', ['$scope', '$http',
 		console.log($scope);
 	}
 ]);
+
+
+
+rebelLeaguesAdminControllers.controller('editGameCtrl', ['$scope', '$http',
+	function ($scope, $http) {
+	
+		$scope.title = "Modifier une partie";
+		$scope.partial = "partials/editGame.html";
+		
+		$scope.expanded = false;
+		$scope.toggle = function () { $scope.expanded = !$scope.expanded };
+		
+		$scope.gameSelected = false;
+	
+		(function() {
+			Date.prototype.toYMDHMS = Date_toYMDHMS;
+			function Date_toYMDHMS() {
+				var year, month, day, hours, minute, seconds;
+				year = String(this.getFullYear());
+				month = String(this.getMonth() + 1);
+				if (month.length == 1) {
+					month = "0" + month;
+				}
+				day = String(this.getDate());
+				if (day.length == 1) {
+					day = "0" + day;
+				}
+				hours = ("00" + this.getHours()).slice(-2);
+				minutes = ("00" + this.getMinutes()).slice(-2);
+				seconds = ("00" + this.getSeconds()).slice(-2);
+				return year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
+			}
+		})();
+		
+		$scope.games = [];
+		$scope.factions = [];
+		$scope.players = [];
+		
+		
+		$http.get('../api/games/all')
+			.then(
+				function success(response) { $scope.games = response.data.data.games; },
+				function error(reason)     { return false; }
+			);
+		$http.get('../api/factions/leafs')
+			.then(
+				function success(response) { $scope.factions = response.data.data.factions; },
+				function error(reason)     { return false; }
+			);
+		$http.get('../api/players/')
+			.then(
+				function success(response) { $scope.players = response.data.data.players; },
+				function error(reason)     { return false; }
+			);
+		
+		$scope.game = {
+			player1_id: null,
+			player1_faction_id: null,
+			player2_id: null,
+			player2_faction_id: null,
+			is_draw: false,
+			is_ranked: true,
+			is_time_runout: false,
+			is_online: false,
+			datetime: new Date(),
+			notes: ""
+		};
+		
+		$scope.game.datetime.setHours( ((($scope.game.datetime.getMinutes()/105 + .5) | 0) + $scope.game.datetime.getHours()) % 24 );
+		$scope.game.datetime.setMinutes( ((($scope.game.datetime.getMinutes() + 7.5)/15 | 0) * 15) % 60 );
+		$scope.game.datetime.setSeconds(0);
+		
+		$scope.submit = function (game) {
+			
+			game.date = game.datetime.toYMDHMS();
+			
+			//console.log(encodeURIComponent(angular.toJson(game)));
+			
+			$http.post('../api/games', game).success( function (data) {
+				alert(data);
+				console.log(data);
+			});
+		
+			console.log(game);
+			console.log(game.datetime.toYMDHMS());
+		};
+		
+		console.log($scope);
+		
+	}
+]);
+
+
 
 
 rebelLeaguesAdminControllers.controller('tier1Ctrl', ['$scope', '$http',
