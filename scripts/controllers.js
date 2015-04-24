fb8eaf5c49b70d5900293e2c2b46965bd98197ab
Mu0n <micjuneau@gmail.com>
2015-04-24 09:38:32 -0400

    removed old playersReview ctrl

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -153,38 +153,6 @@ rebelLeaguesControllers.controller('playerReviewCtrl', ['$scope', '$http',
 	}
 ]);
 
-rebelLeaguesControllers.controller('playerReviewCtrl', ['$scope', '$http',function($scope, $http) {
-		
-		$http.get('api/players/6/efficiencyRatiosWith')
-			.then(
-				function success(response) { $scope.efficiencyRatiosWith = response.data.data; },
-				function error(reason) {return false; }
-				)
-		$http.get('api/players/6/efficiencyRatiosAgainst')
-			.then(
-				function success(response) { $scope.efficiencyRatiosAgainst = response.data.data; },
-				function error(reason) {return false; }
-				)
-		$http.get('api/players/6')
-			.then(
-				function success(response) { $scope.players = response.data.data; },
-				function error(reason) {return false; }
-				);
-		$http.get('api/players/6/lastdate')
-			.then(
-				function success(response) { 
-				    var treatThis = response.data.data.lastdate;
-				    var date = new Date(treatThis.substring(0,4), parseInt(treatThis.substring(5,7))-1, treatThis.substring(8,10) );
-					
-					$scope.lastdate = date.getDate() + " " + monthNames[date.getMonth()] + " " + date.getFullYear();
-					},
-				function error(reason) {return false; }
-				);
-		console.log($scope);
-    //just a test for mic centric view	
-	}
-]);
-
 rebelLeaguesControllers.controller('playersRankingCtrl', ['$scope', '$http', '$modal',
 	function ($scope, $http, $modal) {
 		$http.get('api/ranking').success(function(data) {
